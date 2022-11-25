c71bb9480ba66e9a6d360cca5447d0ca91f15153
NAMRATA <namratajhait@gmail.com>
2022-11-25 19:22:48 +0530

    my message

--- src/components/Signupform/index.js
+++ src/components/Signupform/index.js
@@ -2,9 +2,27 @@ import React, { useState} from "react";
 import './signupform.css';
 
 export default function Signupform() {
-    const[fname, setFName] = useState(false)
+    
+    const[lname, setLname] = useState(false)
 
-    const[fnameError, setFnameError] = useState(false)
+    const[lnameError, setLnameError] = useState(false)
+
+    const[email, setEmail] = useState(false)
+
+    const [EmailError, setEmailError] = useState(false)
+
+    const [password, setPassword] = useState('')
+
+    const [rePassword, setRePassword] = useState('')
+    const [ passwordMatchingError, setPasswordMatchingError] =useState(false);
+
+    const[forgotPass,  setForgotPass] = useState(false)
+    const[forgotPassError,  setForgotPassError,] = useState(false);
+
+
+    const[fname,  setFName] = useState(false)
+
+    const[fnameError,  setFnameError,] = useState(false)
 
     const handleSubmit =(e) => {
         e.preventDefault();
@@ -17,63 +35,53 @@ export default function Signupform() {
         if (!email) setEmailError(true)
         else setEmailError(false)
 
-        if (!password) setPasswordError(true)
-        else setPasswordError(false)
+        if (password !== rePassword) setPasswordMatchingError(true)
+        else setPasswordMatchingError(false)
 
-        if (!retypePass) setRetypePassError(true)
-        else setRetypePassError(false)
-
+        if (!forgotPass) setForgotPassError(true)
+        else setForgotPassError(false)
     }
-
-    const[lname, setLname] = useState(false)
-
-    const[lnameError, setLnameError] = useState(false)
-
-    const[email, setEmail] = useState(false)
-
-    const[EmailError, setEmailError] = useState(false)
-
-    const[password, setPassword] = useState(false)
-
-    const[passwordError, setPasswordError] = useState(false)
-
-    const[retypePass, setRetypePass] = useState(false)
-
-    const[RetypePassError, setRetypePassError] = useState(false)
-
     return (
         <div className="sign-up-container">
             <h1>CREATE ACCOUNT</h1>
             <form onSubmit={handleSubmit}>
+              <div className="form-row">
                 <div className="col-md">
                     <input type="text" className="form-control" placeholder="first Name"  onBlur={(e) => setFName(e.target.value)} />
                     {fnameError && <small className="text-danger"> first Name should not be empty</small>}
+              </div>
                 </div>
-                <div className="col-md">
-                    <div className="col-md">
+                <div className="form-row">
+                  <div className="col-md">
                         <input type="text" className="form-control" placeholder="Last Name" onBlur={(e) => setLname(e.target.value)} />
                         {lnameError && <small className="text-danger"> Last Name should not be empty</small>}
-                    </div>
+                </div>    
                 </div>
+                <div className="form-row">
                 <div className="col-md">
-                    <div className="col-md">
                         <input type="text" className="form-control" placeholder="Email" onBlur={(e) => setEmail(e.target.value)} />
                         {EmailError && <small className="text-danger">Email Name should not be empty</small>}
+                </div>    
+                </div>
+                <div className="form-row">
+                    <div className="col-md">
+                        <input type="text" className="form-control" placeholder="password" onBlur={(e) => setPassword(e.target.value)} />
                     </div>
                 </div>
+                <div className="form-row">
                 <div className="col-md">
-                    <div className="col-md">
-                        <input type="text" className="form-control" placeholder="password"  onBlur={(e) => setPassword(e.target.value)} />
-                        {passwordError && <small className="text-danger">Entered your password</small>}
-                    </div>
+                        <input type="password" className="form-control" placeholder="Re-type Password" onBlur={(e) => setRePassword(e.target.value)} />
                 </div>
-                <div className="col-md">
-                    <div className="col-md">
-                        <input type="text" className="form-control" placeholder="Re type password" onBlur={(e) => setRetypePass(e.target.value)} />
-                        {RetypePassError && <small className="text-danger">Re-type your password</small>}
-                    </div>
+                </div>
+                <div className="app">
+                    <input type="forgotpass" className="form-control" placeholder="Forgot Password" onBlur={(e) => setForgotPass(e.target.value)} />
+                    { forgotPassError}
                 </div>
                 <button>SIGN UP</button>
+                {passwordMatchingError && <small className="text-danger">password is not matching</small>}
+                <div className="login">
+                    <button>SIGN IN</button>
+                </div>
 
             </form>
         </div>
